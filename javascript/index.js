9d2a2b50c60936b95846caaaf706737ce4460876
Bishop2592 <86909488+Bishop2592@users.noreply.github.com>
2021-07-08 17:32:04 +0100

    Add files via upload

---
+++ javascript/index.js
@@ -0,0 +1,83 @@
+// assignment
+// number 1
+// let x = 100;
+// let y = 300;
+// console.log(x, y);
+// window.alert(y);
+
+
+// number 2
+// let a = 3;
+// let b = -7;
+// let c = 2;
+// let d = a * b * c;
+// console.log(d);
+// window.alert(d);
+
+// number 3
+// let a = 0;
+// let b = -1;
+// let c = 4;
+// console.log(c, a, b);
+// window.alert(c, a, b);
+
+// number 4
+// let a=-5;
+// let b=-2;
+// let c=-6;
+// let d= 0;
+// let f=-1;
+// if (a>b && a>c && a>d && a>f)
+// {
+//     console.log(a);
+//     window.alert(a);
+// }
+// else if (b>a && b>c && b>d && b>f)
+// {
+//     console.log(b);
+//     window.alert(b);
+// }
+// else if (c>a && c>b && c>d && c>f)
+// {
+//     console.log(c);
+//     window.alert(c);
+// }
+// else if (d>a && d>c && d>b && d>f)
+// {
+//     console.log(d);
+//     window.alert(d);
+// }
+// else
+// {
+//     console.log(f);
+//     window.alert(f);
+// }
+
+
+// number 5
+// for (let x=0; x<=15; x++)
+// if (x === 0) {
+//            console.log(x +  " is even");
+// }
+// else if (x % 2 === 0) {
+//            console.log(x + " is even");   
+// }
+// else {
+//            console.log(x + " is odd");
+// }
+
+
+// number 6
+for (let a = 1; a <= 100; a++)
+if (a % 3 === 0 && a % 5 === 0) {
+    console.log(a + " FizzBuzz");
+}
+else if (a % 3 === 0) {
+    console.log(a + " Fizz");
+}
+else if (a % 5 === 0) {
+    console.log(a + " Buzz");
+}
+else {
+    console.log(a);
+}
